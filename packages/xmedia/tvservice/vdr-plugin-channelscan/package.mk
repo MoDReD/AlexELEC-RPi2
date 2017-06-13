b185906e3210bc65b0f09ca4b1d808673c561fb3
alexelec <alexelecv5@gmail.com>
2017-06-13 18:50:55 +0300

    vdr-plugin-channelscan: bump to 8b67baa

--- packages/xmedia/tvservice/vdr-plugin-channelscan/package.mk
+++ packages/xmedia/tvservice/vdr-plugin-channelscan/package.mk
@@ -1,32 +1,27 @@
 ################################################################################
 #      This file is part of Alex@ELEC - http://www.alexelec.in.ua
-#      Copyright (C) 2011-2016 Alexandr Zuyev (alex@alexelec.in.ua)
+#      Copyright (C) 2011-2017 Alexandr Zuyev (alex@alexelec.in.ua)
 ################################################################################
 
 PKG_NAME="vdr-plugin-channelscan"
-PKG_VERSION="0.6.2-mod-9"
+PKG_VERSION="8b67baa"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="http://linuxdvb.org.ru/wbb/index.php?page=Thread&threadID=1745"
+PKG_SITE="https://bitbucket.org/ua0lnj/channelscan"
 PKG_URL="$ALEXELEC_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain vdr"
-PKG_PRIORITY="optional"
 PKG_SECTION="xmedia/tvservice"
 PKG_SHORTDESC="VDR -- channel scan plugin"
 PKG_LONGDESC="channelscan is a plugin for VDR, the Video Disk Recorder."
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+PKG_LOCALE_INSTALL="yes"
 
 pre_configure_target() {
   export CFLAGS="$CFLAGS -fPIC"
   export CXXFLAGS="$CXXFLAGS -fPIC"
   export LDFLAGS="$LDFLAGS -fPIC"
-}
-
-pre_make_target() {
-  # dont build parallel
-  MAKEFLAGS=-j1
 }
 
 make_target() {
@@ -44,22 +39,4 @@ makeinstall_target() {
 post_makeinstall_target() {
   mkdir -p $INSTALL/usr/config/vdr/plugins/transponders
     cp $PKG_DIR/transponders/* $INSTALL/usr/config/vdr/plugins/transponders
-}
-
-post_pkginstall_target() {
-  for i in $INSTALL $INSTALL/usr; do
-      rm -rf $i/include
-      rm -rf $i/lib/pkgconfig
-      rm -rf $i/share/man
-      find $i -name "*.la" -exec rm -f "{}" ";" 2>/dev/null || true
-      find $i -name "*.a" -exec rm -f "{}" ";" 2>/dev/null || true
-      find $i -name "*.so*T" -exec rm -f "{}" ";" 2>/dev/null || true
-      # patch backups nonsense
-      find $i -name "*.orig" -exec rm -f "{}" ";" 2>/dev/null || true
-  done
-
-  find $INSTALL -type d -exec rmdir -p "{}" ";" 2>/dev/null || true
-
-  $STRIP `find $INSTALL -name "*.so" 2>/dev/null` 2>/dev/null || :
-  $STRIP `find $INSTALL -name "*.so.[0-9]*" 2>/dev/null` 2>/dev/null || :
 }
