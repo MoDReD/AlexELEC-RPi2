6d3ce801707793a7c182aae4be0ded687122a7fc
alexelec <alexelecv5@gmail.com>
2017-06-13 18:06:26 +0300

    minidlna: bump to d1cd7c7

--- packages/xmedia/tools/minidlna/package.mk
+++ packages/xmedia/tools/minidlna/package.mk
@@ -1,35 +1,35 @@
 ################################################################################
 #      This file is part of Alex@ELEC - http://www.alexelec.in.ua
-#      Copyright (C) 2011-2016 Alexandr Zuyev (alex@alexelec.in.ua)
+#      Copyright (C) 2011-2017 Alexandr Zuyev (alex@alexelec.in.ua)
 ################################################################################
 
 PKG_NAME="minidlna"
-PKG_VERSION="4ec6cd0"
+PKG_VERSION="d1cd7c7"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://minidlna.sourceforge.net/"
 PKG_URL="$ALEXELEC_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain sqlite libexif libid3tag flac"
-PKG_PRIORITY="optional"
+PKG_DEPENDS_TARGET="toolchain sqlite libexif libid3tag flac libvorbis"
 PKG_SECTION="xmedia/tools"
 PKG_SHORTDESC="MiniDLNA is server software with the aim of being fully compliant with DLNA/UPnP-AV clients"
 PKG_LONGDESC="MiniDLNA is server software with the aim of being fully compliant with DLNA/UPnP-AV clients."
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
+pre_configure_target() {
+ # minidlna fails to build in subdirs
+ cd $ROOT/$PKG_BUILD
+ rm -rf .$TARGET_NAME
+}
+
 PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr \
 			               --sbindir=/usr/bin \
-                           --with-os-name="Alex@ELEC" \
+                           --with-os-name="AlexELEC" \
 			               --with-os-version="$ALEXELEC_VERSION" \
 			               --with-os-url="www.alexelec.in.ua" \
 			               --with-log-path=/var/log \
 			               --with-db-path=/storage/.cache/minidlna"
-
-pre_configure_target() {
-  cd $ROOT/$PKG_BUILD
-    cp -rf * .$TARGET_NAME
-}
 
 post_makeinstall_target() {
   mkdir -p $INSTALL/usr/config/minidlna
