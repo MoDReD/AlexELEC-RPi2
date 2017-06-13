41a549b47aa0960f3f4cb0916331722ac4934693
alexelec <alexelecv5@gmail.com>
2017-06-13 20:29:19 +0300

    tvheadend: bump to v4.2.2-35-g60e4409

--- packages/xmedia/tvservice/tvheadend/package.mk
+++ packages/xmedia/tvservice/tvheadend/package.mk
@@ -1,60 +1,74 @@
 ################################################################################
 #      This file is part of Alex@ELEC - http://www.alexelec.in.ua
-#      Copyright (C) 2011-2016 Alexandr Zuyev (alex@alexelec.in.ua)
+#      Copyright (C) 2011-2017 Alexandr Zuyev (alex@alexelec.in.ua)
 ################################################################################
 
 PKG_NAME="tvheadend"
-PKG_VERSION="4c75447"
-TVH_VERSION="4.1.1708"
+PKG_VERSION="60e4409"
 PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.tvheadend.org"
-PKG_URL="$ALEXELEC_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain libressl curl libdvbcsa"
+PKG_URL=""
+PKG_DEPENDS_TARGET="toolchain curl libdvbcsa libiconv libressl pngquant:host Python:host yasm jq"
 PKG_PRIORITY="optional"
 PKG_SECTION="xmedia/tvservice"
 PKG_SHORTDESC="Tvheadend: a TV streaming server for Linux supporting DVB-S, DVB-S2, DVB-C, DVB-T, ATSC, IPTV, and Analog video (V4L) as input sources."
 PKG_LONGDESC="Tvheadend is a TV streaming server for Linux supporting DVB-S, DVB-S2, DVB-C, DVB-T, ATSC, IPTV, and Analog video (V4L) as input sources. It also comes with a powerful and easy to use web interface both used for configuration and day-to-day operations, such as searching the EPG and scheduling recordings. Even so, the most notable feature of Tvheadend is how easy it is to set up: Install it, navigate to the web user interface, drill into the TV adapters tab, select your current location and Tvheadend will start scanning channels and present them to you in just a few minutes. If installing as an Addon a reboot is needed"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+PKG_LOCALE_INSTALL="yes"
 
-post_unpack() {
-  sed -e 's/VER="0.0.0~unknown"/VER="'$TVH_VERSION'~g'$PKG_VERSION'~AlexELEC"/g' -i $PKG_BUILD/support/version
+unpack() {
+  git clone -b 'release/4.2' https://github.com/tvheadend/tvheadend.git $PKG_BUILD
+  cd $PKG_BUILD
+  git reset --hard $PKG_VERSION
+  PKG_VERSION_NUMBER=`git describe --match "v*"`
+  echo "****** version: $PKG_VERSION_NUMBER ******"
+  sed -e 's/VER="0.0.0~unknown"/VER="'$PKG_VERSION_NUMBER' ~ Alex@ELEC"/g' -i support/version
+  sed -e 's|'/usr/bin/pngquant'|'$ROOT/$TOOLCHAIN/bin/pngquant'|g' -i support/mkbundle
+  rm -rf .git
+  cd $ROOT
 }
 
-pre_build_target() {
-  mkdir -p $PKG_BUILD/.$TARGET_NAME
-  cp -RP $PKG_BUILD/* $PKG_BUILD/.$TARGET_NAME
+PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr \
+                           --arch=$TARGET_ARCH \
+                           --cpu=$TARGET_CPU \
+                           --cc=$CC \
+                           --disable-avahi \
+                           --disable-dbus_1 \
+                           --disable-uriparser \
+                           --disable-dvbscan \
+                           --disable-hdhomerun_client \
+                           --disable-hdhomerun_static \
+                           --disable-nvenc \
+                           --disable-ffmpeg_static \
+                           --disable-libav \
+                           --disable-bintray_cache \
+                           --enable-dvbcsa \
+                           --enable-tvhcsa \
+                           --enable-dvben50221 \
+                           --enable-bundle \
+                           --enable-trace \
+                           --enable-timeshift \
+                           --enable-epoll \
+                           --enable-inotify \
+                           --enable-pngquant \
+                           --nowerror \
+                           --python=$ROOT/$TOOLCHAIN/bin/python"
+
+pre_configure_target() {
+# fails to build in subdirs
+  cd $ROOT/$PKG_BUILD
+    rm -rf .$TARGET_NAME
+
   export CROSS_COMPILE=$TARGET_PREFIX
-}
-
-configure_target() {
-  ./configure --prefix=/usr \
-            --arch=$TARGET_ARCH \
-            --cpu=$TARGET_CPU \
-            --cc=$TARGET_CC \
-            --disable-hdhomerun_client \
-            --disable-hdhomerun_static \
-            --disable-avahi \
-            --disable-libav \
-            --disable-libffmpeg_static \
-            --enable-timeshift \
-            --enable-inotify \
-            --enable-epoll \
-            --disable-uriparser \
-            --disable-dvbscan \
-            --enable-tvhcsa \
-            --enable-bundle \
-            --platform=linux \
-            --enable-dvbcsa \
-            --disable-dbus_1 \
-            --python=$ROOT/$TOOLCHAIN/bin/python \
-            --nowerror
+  export CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include/iconv -L$SYSROOT_PREFIX/usr/lib/iconv"
 }
 
 post_make_target() {
   $CC -O -fbuiltin -fomit-frame-pointer -fPIC -shared -o capmt_ca.so src/extra/capmt_ca.c -ldl
+  $STRIP $ROOT/$PKG_BUILD/build.linux/tvheadend
 }
 
 post_makeinstall_target() {
@@ -66,30 +80,13 @@ post_makeinstall_target() {
     cp -P support/sat_xml_scan.py $INSTALL/usr/bin/sat_xml_scan
 
   #sat files
-  mkdir -p  $INSTALL/usr/config/tvheadend
-    cp -a $PKG_DIR/config/* $INSTALL/usr/config/tvheadend
+  mkdir -p $INSTALL/usr/config/tvheadend
   mkdir -p $INSTALL/usr/config/tvheadend/dvb-scan
     cp -a data/dvb-scan/atsc $INSTALL/usr/config/tvheadend/dvb-scan
     cp -a data/dvb-scan/dvb-c $INSTALL/usr/config/tvheadend/dvb-scan
     cp -a data/dvb-scan/dvb-s $INSTALL/usr/config/tvheadend/dvb-scan
     cp -a data/dvb-scan/dvb-t $INSTALL/usr/config/tvheadend/dvb-scan
     cp -a data/dvb-scan/isdb-t $INSTALL/usr/config/tvheadend/dvb-scan
-}
-
-post_pkginstall_target() {
-  for i in $INSTALL $INSTALL/usr; do
-      rm -rf $i/include
-      rm -rf $i/lib/pkgconfig
-      rm -rf $i/share/man
-      find $i -name "*.la" -exec rm -f "{}" ";" 2>/dev/null || true
-      find $i -name "*.a" -exec rm -f "{}" ";" 2>/dev/null || true
-      find $i -name "*.so*T" -exec rm -f "{}" ";" 2>/dev/null || true
-      # patch backups nonsense
-      find $i -name "*.orig" -exec rm -f "{}" ";" 2>/dev/null || true
-  done
-
-  find $INSTALL -type d -exec rmdir -p "{}" ";" 2>/dev/null || true
-
-  $STRIP `find $INSTALL -name "*.so" 2>/dev/null` 2>/dev/null || :
-  $STRIP `find $INSTALL -name "*.so.[0-9]*" 2>/dev/null` 2>/dev/null || :
+  #config
+    cp -a $PKG_DIR/config/* $INSTALL/usr/config/tvheadend
 }
