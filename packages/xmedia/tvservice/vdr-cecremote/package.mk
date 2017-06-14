f03f343c31e48bbddaf34b927b7063e139844f7f
alexelec <alexelecv5@gmail.com>
2017-06-14 08:14:29 +0300

    vdr-cecremote: set PKG_LOCALE_INSTALL

--- packages/xmedia/tvservice/vdr-cecremote/package.mk
+++ packages/xmedia/tvservice/vdr-cecremote/package.mk
@@ -17,6 +17,7 @@ PKG_SHORTDESC="Receive and send commands via CEC"
 PKG_LONGDESC="Receive and send commands via CEC"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+PKG_LOCALE_INSTALL="yes"
 
 pre_configure_target() {
   export CFLAGS="$CFLAGS -fPIC"
@@ -32,24 +33,5 @@ make_target() {
 post_makeinstall_target() {
   mkdir -p $INSTALL/usr/config
     mv -f $INSTALL/storage/.config/vdr $INSTALL/usr/config/vdr
-}
-
-post_pkginstall_target() {
-  for i in $INSTALL $INSTALL/usr; do
-      rm -rf $i/include
-      rm -rf $i/lib/pkgconfig
-      rm -rf $i/share/man
-      find $i -name "*.la" -exec rm -f "{}" ";" 2>/dev/null || true
-      find $i -name "*.a" -exec rm -f "{}" ";" 2>/dev/null || true
-      find $i -name "*.so*T" -exec rm -f "{}" ";" 2>/dev/null || true
-      # patch backups nonsense
-      find $i -name "*.orig" -exec rm -f "{}" ";" 2>/dev/null || true
-  done
-
-  find $INSTALL -type d -exec rmdir -p "{}" ";" 2>/dev/null || true
-
-  $STRIP `find $INSTALL -name "*.so" 2>/dev/null` 2>/dev/null || :
-  $STRIP `find $INSTALL -name "*.so.[0-9]*" 2>/dev/null` 2>/dev/null || :
-
   rm -rf $INSTALL/storage
 }
