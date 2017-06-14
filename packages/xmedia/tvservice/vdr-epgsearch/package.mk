95d79e6e50b9b26e51fb52dde33d4d72129240cf
alexelec <alexelecv5@gmail.com>
2017-06-14 08:39:17 +0300

    vdr-epgsearch: set PKG_LOCALE_INSTALL

--- packages/xmedia/tvservice/vdr-epgsearch/package.mk
+++ packages/xmedia/tvservice/vdr-epgsearch/package.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #      This file is part of Alex@ELEC - http://www.alexelec.in.ua
-#      Copyright (C) 2011-2016 Alexandr Zuyev (alex@alexelec.in.ua)
+#      Copyright (C) 2011-2017 Alexandr Zuyev (alex@alexelec.in.ua)
 ################################################################################
 
 PKG_NAME="vdr-epgsearch"
@@ -9,7 +9,6 @@ PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://projects.vdr-developer.org/git/vdr-plugin-epgsearch.git/"
-#PKG_URL="$ALEXELEC_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain vdr"
 PKG_PRIORITY="optional"
@@ -18,6 +17,7 @@ PKG_SHORTDESC="VDR -- epgsearch plugin"
 PKG_LONGDESC="vdr-epgsearch is a plugin for VDR, the Video Disk Recorder."
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+PKG_LOCALE_INSTALL="yes"
 
 pre_configure_target() {
   export CFLAGS="$CFLAGS -fPIC -L$SYSROOT_PREFIX/usr/lib -L$SYSROOT_PREFIX/lib"
@@ -33,24 +33,5 @@ make_target() {
 post_makeinstall_target() {
   mkdir -p $INSTALL/usr/config/vdr/plugins/epgsearch
     cp -PR $PKG_DIR/config/* $INSTALL/usr/config/vdr/plugins/epgsearch
-}
-
-post_pkginstall_target() {
-  for i in $INSTALL $INSTALL/usr; do
-      rm -rf $i/include
-      rm -rf $i/lib/pkgconfig
-      rm -rf $i/share/man
-      find $i -name "*.la" -exec rm -f "{}" ";" 2>/dev/null || true
-      find $i -name "*.a" -exec rm -f "{}" ";" 2>/dev/null || true
-      find $i -name "*.so*T" -exec rm -f "{}" ";" 2>/dev/null || true
-      # patch backups nonsense
-      find $i -name "*.orig" -exec rm -f "{}" ";" 2>/dev/null || true
-  done
-
-  find $INSTALL -type d -exec rmdir -p "{}" ";" 2>/dev/null || true
-
-  $STRIP `find $INSTALL -name "*.so" 2>/dev/null` 2>/dev/null || :
-  $STRIP `find $INSTALL -name "*.so.[0-9]*" 2>/dev/null` 2>/dev/null || :
-
   rm -rf $INSTALL/storage
 }
