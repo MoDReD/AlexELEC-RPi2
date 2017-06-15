47a616c9a733e5cfc62b20eb29a22d6869f297ec
alexelec <alexelecv5@gmail.com>
2017-06-15 14:55:46 +0300

    rpi.advanced.settings: fix services

--- packages/mediacenter/rpi.advanced.settings/package.mk
+++ packages/mediacenter/rpi.advanced.settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="rpi.advanced.settings"
-PKG_VERSION="9f9fe35"
+PKG_VERSION="381d298"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
